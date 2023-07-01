1ae9bfa31e38d6be0656b6d0084a03f50f2fbc13
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-07-01 18:04:12 +0200

    intial values provided project ready for developnt

---
+++ movie_watchlist/__init__.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from pymongo import MongoClient
+
+from movie_watchlist.routes import pages
+
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["MONGODB_URI"] = os.getenv("MONGODB_URI")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+    app.register_blueprint(pages)
+    
+    return app
